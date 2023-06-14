0ed1579f43b91d623f223bb64d780a1fa8d20c4a
kriswils60 <133425984+kriswils60@users.noreply.github.com>
2023-06-14 14:56:06 -0400

    Completes the reverseString exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(textToReverse) {
+    const arrayHolder = textToReverse.split("");
+    const arrayLength = arrayHolder.length;
+    let output = '';
 
+    for (let i = 1; i <= arrayLength; i++) {
+
+        output += arrayHolder[arrayLength - i];
+
+    }
+
+    return output;
 };
 
 // Do not edit below this line
