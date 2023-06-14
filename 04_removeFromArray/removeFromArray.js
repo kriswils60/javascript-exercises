968d2d0afdcade8c739018d1fa26d19d8fab41ac
kriswils60 <133425984+kriswils60@users.noreply.github.com>
2023-06-14 15:25:06 -0400

    Completes the removeFromArray exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(suppliedArray, ...itemsToRemove) {
 
+    for(const item in itemsToRemove) {
+        for(let i = 0; i < suppliedArray.length; i++) {
+            if(itemsToRemove[item] === suppliedArray[i]) {
+                suppliedArray.splice(i, 1);
+            }
+        }
+    }
+
+    return suppliedArray;
 };
 
 // Do not edit below this line
