1258c900c2d4be034660c80463ecebc519fbcb07
kriswils60 <133425984+kriswils60@users.noreply.github.com>
2023-06-14 14:30:55 -0400

    Completes the repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(theString, repeatTimes) {
+    let output = '';
+
+    if(repeatTimes >= 0) {
+        for (let i = 0; i < repeatTimes; i++) {
+            output += theString;
+        }
+    } else {
+        output = 'ERROR';
+    }
+
+    return output;
 
 };
 
