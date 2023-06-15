9694d5c72b0552b969916d981ef6ce01de648b8d
kriswils60 <133425984+kriswils60@users.noreply.github.com>
2023-06-15 16:00:56 -0400

    Completes the sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let output = 0,
+        largerNum = number2,
+        smallerNum = number1;
 
+    if(typeof number1 === "number" && typeof number2 === "number" && number1 >= 0 && number2 >= 0) {
+
+        if (number1 > number2) {
+            largerNum = number1;
+            smallerNum = number2;
+        }
+
+        for(let i = smallerNum; i <= largerNum; i++) {
+            output += i;
+        }
+
+    } else output = 'ERROR';
+
+    return output;
 };
-
+// 1,3 = 1 + 2 + 3
+// 4,1 = 1 + 2 + 3 + 4
+// 1,1 = 1 + 1
 // Do not edit below this line
 module.exports = sumAll;
