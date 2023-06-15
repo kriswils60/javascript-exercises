ad626a50face705535c7025fb8007ad64804e803
kriswils60 <133425984+kriswils60@users.noreply.github.com>
2023-06-15 16:31:16 -0400

    Completes the tempConversion exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  let output;
+  output = ((farenheitTemp - 32) * 5/9);
+  
+  if(output % 1 !== 0) {
+    output = parseFloat(output.toFixed(1));
+  }
+  
+  return output;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let output;
+
+  output =  ((celsiusTemp * 9/5) + 32);
+  
+  if(output % 1 !== 0) {
+    output = parseFloat(output.toFixed(1));
+  }
+
+  return output;
 };
 
 // Do not edit below this line
